8c1b62be5b922245a32846b5122872ac9665cf16
Bryn Davies <bryn.davies@admiralfinalcialservices.co.uk>
2024-12-07 12:46:55 +0000

    solve 2

--- 6/main.py
+++ 6/main.py
@@ -24,58 +24,28 @@ def cell_at(pos, map_2d_array):
 def walk_map(map_2d_array, pos, dir, debug_draw=False):
     cells_visited = 1 # Start position
     next_pos = pos + dir
-    while not out_of_bounds(next_pos, map_2d_array):
-        if cell_at(next_pos, map_2d_array) == '#':
-            dir.rotate_90()
-        else:
-            pos += dir
-            if cell_at(pos,map_2d_array) == '.':
-                cells_visited += 1
-                map_2d_array[pos.y][pos.x] = 'X'
-        next_pos = pos + dir
-        if debug_draw: draw_map(map_2d_array)
-        
-    print(cells_visited)
-
-def place_printing_press(map_2d_array, pos, dir, debug_draw=False):
-    loopable_places = 0
-    cells_visited = 1 # Start position
-    next_pos = pos + dir
     map_2d_array[pos.y][pos.x] = 'X'
-    map_2d_history = [[Point2D(0, 0) for _ in row] for row in map_2d_array]
+    chosen_path = [Point2D(dir.x, dir.y)]
+    rot_count = 0
     while not out_of_bounds(next_pos, map_2d_array):
-        
-                
         if cell_at(next_pos, map_2d_array) == '#':
             dir.rotate_90()
+            rot_count += 1
         else:
             pos += dir
             if cell_at(pos,map_2d_array) == '.':
                 cells_visited += 1
                 map_2d_array[pos.y][pos.x] = 'X'
-                map_2d_history[pos.y][pos.x] = Point2D(dir.x, dir.y)
+                chosen_path.append(Point2D(pos.y, pos.x))
+                rot_count = 0
         
-
-        #if turning right puts me on a path I've already visited (in the same direction)
-        # I would be in a loop if I put a Printing Press in front of me.
-        if cell_at(next_pos, map_2d_array) != '#':
-            probe_dir = Point2D(dir.x, dir.y)
-            probe_dir.rotate_90()
-            probe_pos = pos + probe_dir
-            while not out_of_bounds(probe_pos, map_2d_array):
-                if cell_at(probe_pos, map_2d_history) == probe_dir:
-                    printing_press_pos = pos + dir
-                    print(f"Printing Press at {printing_press_pos}")
-                    loopable_places += 1
-                    break 
-                probe_pos = probe_pos + probe_dir
         if debug_draw: draw_map(map_2d_array)
-
         next_pos = pos + dir
+        if rot_count > 4:
+            print('Loop detected')
+            return None
 
-        
-    print(cells_visited)
-    print(loopable_places)
+    return chosen_path
 
 def puzzle1():
     map_2d_array, pos = load_map('input.txt')
@@ -87,6 +57,20 @@ def puzzle2():
     dir = Point2D(0, -1)
     #if turning right puts me on a path I've already visited (in the same direction)
     # I would be in a loop if I put a Printing Press in front of me.
-    place_printing_press(map_2d_array, pos, dir, debug_draw=False)
-
+    path_walked = walk_map(map_2d_array, pos, dir, debug_draw=False)
+    steps = len(path_walked)
+    print('steps ' + str(steps))
+    num_danger_nodes = 0
+    for enum, cell in enumerate(path_walked):
+        print('percentage ' + str((enum/steps) * 100))
+        if enum == 0:
+            continue
+        map, pos = load_map('input.txt')
+        map[cell.x][cell.y] = '#'
+        dir = Point2D(0, -1)
+        length_of_walk = walk_map(map, pos, dir, debug_draw=False)
+        if length_of_walk is None:
+            num_danger_nodes += 1
+        
+    print('num_danger_nodes ' + str(num_danger_nodes)) 
 puzzle2()
